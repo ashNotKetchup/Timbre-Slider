18d0564a3ce3b93cf8e81d2809586e62fb007a78
Ash <Ashleynh@hotmail.co.uk>
2026-05-04 15:17:57 +0100

    bit more cleaning

--- export.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Wrapper script to run export from the backend package.
-"""
-import sys
-from backend.export import main
-
-if __name__ == "__main__":
-    sys.exit(main())
--- mass_preprocess.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Wrapper script to run mass_preprocess from the backend package.
-"""
-import sys
-from backend.utilities.mass_preprocess import main
-
-if __name__ == "__main__":
-    main()
